7c67426e275b54c6f659e677a3e5686479873d80
haifaamalinawahdan <haifanew3@gmail.com>
2026-05-11 13:11:48 +0700

    membuat prosedur mencetak daftar anak

--- ibu.h
+++ ibu.h
@@ -25,6 +25,17 @@ public:
     {
         daftar_anak.push_back(pAnak);
     }
+    void cetakAnak()
+    {
+        cout << "Daftar Anak dari Ibu \"" << this->nama << "\":\n";
+
+        for (int i = 0; i < daftar_anak.size(); i++)
+        {
+            cout << daftar_anak[i]->nama << endl;
+        }
+
+        cout << endl;
+    }
 
 
 };
