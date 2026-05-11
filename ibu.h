8825aa9c91c641ff9661250a749cea11559ce2b6
haifaamalinawahdan <haifanew3@gmail.com>
2026-05-11 13:09:36 +0700

    membuat constructor ibu

--- ibu.h
+++ ibu.h
@@ -13,6 +13,13 @@ public:
     string nama;
     vector<anak*> daftar_anak;
 
+        ibu(string pNama) : nama(pNama)
+    {
+        cout << "Ibu \"" << nama << "\" ada\n";
+    }
+
+    
+
 
 };
 #endif
